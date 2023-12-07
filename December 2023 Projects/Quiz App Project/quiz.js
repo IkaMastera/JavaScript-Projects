f5636140325722d00b6c1246f4520966ea27db7c
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-12-07 13:50:18 +0400

    add: selectAnswer funciton

--- December 2023 Projects/Quiz App Project/quiz.js
+++ December 2023 Projects/Quiz App Project/quiz.js
@@ -62,6 +62,10 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     })
 }
 
@@ -69,6 +73,16 @@ const resetState = () => {
     nextButton.style.display = "none";
     while(answerButtons.firstChild){
       answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+const selectAnswer = (e) => {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
     }
 }
 
