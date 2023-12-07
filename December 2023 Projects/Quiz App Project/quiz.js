54a5fd165bcd261a5cc8aaac8f6c12078ef22701
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-12-07 12:58:54 +0400

    add: js file and questions data

---
+++ December 2023 Projects/Quiz App Project/quiz.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "Who Founded Tbilisi ?",
+        answers: [
+            { text: "Vaxtang Gorgasali", correct: true},
+            { text: "Shota Rustaveli", correct: false},
+            { text: "Davit Agmashenebeli", correct: false},
+            { text: "Tamar Mefe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the most popular street in Tbilisi ?",
+        answers: [
+            { text: "Pekini", correct: false},
+            { text: "Rustaveli", correct: true},
+            { text: "Xiliani", correct: false},
+            { text: "Vaja-Fshavela", correct: false},
+        ]
+    },
+    {
+        question: "What was old Tbilisi called originally ?",
+        answers: [
+            { text: "Seydabad", correct: true},
+            { text: "Abanotubani", correct: false},
+            { text: "caravanseriais", correct: false},
+            { text: "Feisadod", correct: false},
+        ]
+    },
+    {
+        question: "When was Tbilisi Founded ?",
+        answers: [
+            { text: "521", correct: false},
+            { text: "467", correct: false},
+            { text: "458", correct: true},
+            { text: "502", correct: false},
+        ]
+    },
+];
