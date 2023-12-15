39387c5360425fe857c7065a056154305fdaf70b
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-12-15 14:19:45 +0400

    add: updateStorage function
    so that the notes will be saved after deleting a note or creating one

--- December 2023 Projects/Notes App Project/notes.js
+++ December 2023 Projects/Notes App Project/notes.js
@@ -3,6 +3,10 @@ const createBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box");
 
+const updateStorage = () =>{
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () =>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -15,5 +19,14 @@ createBtn.addEventListener("click", () =>{
 notesContainer.addEventListener("click", (e) =>{
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        updateStorage();
+    }else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
-})
+});
+
